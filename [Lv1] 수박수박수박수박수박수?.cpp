d446a5ae6a25e08b5e8b0f0b3a8ebd4e902f196c
lbg030 <lee971009@naver.com>
2024-06-30 19:01:37 +0900

    [Lv1] 수박수박수박수박수박수?

--- [Lv1] 수박수박수박수박수박수?.cpp
+++ [Lv1] 수박수박수박수박수박수?.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string solution(int n) {
     string answer = "";
     string watermelon[2] = {"수", "박"};
-    // bool div_two;
+    // bool div_two; 
     
     for(int i = 0; i<n; ++i){
         // div_two = (i % 2 == 0) ? True : False;
