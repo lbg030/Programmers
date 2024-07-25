5be9be9cab9f534d99d80bea054a2b1d3b483ea1
lbg030 <lee971009@naver.com>
2024-07-25 14:12:21 +0900

    2024.07.25

--- [2023 KAKAO BLIND RECRUITMENT] 개인 정보 수집 유효기간.cpp
+++ [2023 KAKAO BLIND RECRUITMENT] 개인 정보 수집 유효기간.cpp
@@ -1,84 +1,63 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <sstream>
-#include <utility>
 
 using namespace std;
 
-vector<string> split(const string text, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::stringstream ss(text);
+vector<string> split(const string& text, char delimiter) {
+    vector<string> tokens;
+    stringstream ss(text);
+    string token;
 
-    while (std::getline(ss, token, delimiter)) {
+    while (getline(ss, token, delimiter)) {
         tokens.push_back(token);
     }
     return tokens;
 }
 
-
-vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
+vector<int> solution(const string& today, const vector<string>& terms, const vector<string>& privacies) {
     vector<int> answer;
-    string tmp;
-    char delimiter = '.';
-    map<string, int> term_map;
+    unordered_map<string, int> term_map;
     
-    vector<string> result = split(today, delimiter);
-    int today_year = stoi(result[0]);
-    int today_month = stoi(result[1]);
-    int today_day = stoi(result[2]);
+    // 현재 날짜 파싱
+    vector<string> today_split = split(today, '.');
+    int today_year = stoi(today_split[0]);
+    int today_month = stoi(today_split[1]);
+    int today_day = stoi(today_split[2]);
     
-    for(const auto& term : terms){
-        vector<string> term_result = split(term, ' ');
-        term_map[term_result[0]] = stoi(term_result[1]);
+    // 용어와 유효 기간 매핑
+    for(const auto& term : terms) {
+        vector<string> term_split = split(term, ' ');
+        term_map[term_split[0]] = stoi(term_split[1]);
     }
     
-    int idx = 0;
-    for(const auto& privacy_elem : privacies){
-        idx++;
-        vector<string> privacy = split(privacy_elem, delimiter);
-        int privacy_year = stoi(privacy[0]);
-        int privacy_month = stoi(privacy[1]);
+    // 개인정보와 날짜 비교
+    for (int i = 0; i < privacies.size(); ++i) {
+        const auto& privacy_elem = privacies[i];
+        vector<string> privacy_split = split(privacy_elem, '.');
+        int privacy_year = stoi(privacy_split[0]);
+        int privacy_month = stoi(privacy_split[1]);
 
-        vector<string> day_term_split = split(privacy[2], ' ');
+        vector<string> day_term_split = split(privacy_split[2], ' ');
         int privacy_day = stoi(day_term_split[0]);
         string privacy_term = day_term_split[1];
- 
-        privacy_month += term_map[privacy_term];
-        while(privacy_month > 12){
+        
+        int term_duration = term_map[privacy_term];
+        privacy_month += term_duration;
+        
+        // 연, 월 계산
+        while (privacy_month > 12) {
             privacy_month -= 12;
             privacy_year++;
         }
-        if(today_year > privacy_year){
-            answer.push_back(idx);
-            continue;
-        }
-
-        else if(today_year == privacy_year){ 
-            if(today_month > privacy_month){
-                answer.push_back(idx);
-                continue;
-                }
-            }
-        else{
-            continue;
-        }
         
-        if(today_month > privacy_month){
-            answer.push_back(idx);
-            continue;
-        }
-        
-        else if(today_month == privacy_month){
-            if(today_day >= privacy_day){
-            answer.push_back(idx);
-            continue;
-            }
-        }
-        else{
-            continue;
+        // 현재 날짜와 비교
+        if (today_year > privacy_year ||
+            (today_year == privacy_year && (today_month > privacy_month || 
+            (today_month == privacy_month && today_day >= privacy_day)))) {
+            answer.push_back(i + 1);
         }
     }
     
