4ca9e3ed32a323662ccc1ee3b4c203c983b4bd7e
lbg030 <lee971009@naver.com>
2024-07-30 12:05:24 +0900

    2024.07.30

--- [소프티어] 8단 변속기.cpp
+++ [소프티어] 8단 변속기.cpp
@@ -1,58 +1,41 @@
 #include<iostream>
 
-
 using namespace std;
+
+int get_digit(const string& str, int index){
+    return str[index] - '0';
+}
+
+string determine(const string& inputs){
+    bool is_ascending = get_digit(inputs, 0) < get_digit(inputs, 2);
+    
+    for(size_t i=2; i<inputs.size(); i += 2){
+        int prev = get_digit(inputs, i-2);
+        int current = get_digit(inputs, i);
+        
+        if(is_ascending){
+            if(current < prev){
+                return "mixed";
+            }
+        }else{
+            if(current > prev){
+                return "mixed";
+            }
+        }
+    }
+    return is_ascending ? "ascending" : "descending";
+}
 
 // Initial Code
 int main(int argc, char** argv)
 {
     string inputs;
-    int prev = 0;
-    int current = 0;
-    bool ascending;
-    bool flag = true;
-    
+    string res;
     getline(cin, inputs);
+    res = determine(inputs);
 
-    prev = inputs[0] - '0';
-    current = inputs[2] - '0';
-    if(current > prev){
-        ascending = true;
-    }else{
-        ascending = false;
-    }
-
-    for(int i=4; i<inputs.size()-2; i+=2){
-        if (ascending){
-            if(current < (inputs[i] - '0')){
-                current = inputs[i] - '0';
-            }
-            else{
-                cout << "mixed" << endl;
-                flag = false;
-                break;
-            }
-        }
-        //descending
-        else{
-            if(current > (inputs[i] - '0')){
-                current = (inputs[i] - '0');
-            }
-            else{
-                cout << "mixed" << endl;
-                flag = false;
-                break;
-            }
-        }
-    }
-    if(flag){
-        if(ascending){
-            cout << "ascending" << endl;
-        }else{
-            cout <<"descending" << endl;
-        }
-    }
-    
+    cout << res << endl;
+        
     return 0;
 }
 
