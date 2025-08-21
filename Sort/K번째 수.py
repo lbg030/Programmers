b3d791db508ee8539b06413f00445c65061b5da9
Byeong Gwon Lee <lee971009@naver.com>
2025-08-21 17:09:11 -0400

    Sorting

--- Sort/K번째 수.py
+++ Sort/K번째 수.py
@@ -1,8 +1,9 @@
-def solution(array, commands):
-    answer = []
-    for cmd in commands:
-        # print(arr)
-        start_idx, end_idx, num = cmd
-        # print(array[start_idx-1:end_idx])
-        answer.append(sorted(array[start_idx-1:end_idx])[num-1])
+def solution(numbers):
+    strs = [str(n) for n in numbers]
+    strs.sort(key=lambda s: s * 4, reverse=True)
+    
+    answer = ''.join(strs)
+    
+    if answer[0] == '0':
+        answer = '0'
     return answer
